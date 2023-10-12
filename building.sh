695fdd2a51dde9f7bb8e7a3290f8defbc1b22b86
MaxDac <massimiliano.dacunzo@hotmail.com>
2023-10-12 16:50:00 +0100

    Update building.sh with new neovim version

--- building.sh
+++ building.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Building the images
-docker build -f gitpod-neovim-tmux.dockerfile -t maxdac/gitpod-neovim:0.9.2-tmux-3.3 .
-docker tag maxdac/gitpod-neovim:0.9.2-tmux-3.3 maxdac/gitpod-neovim:latest 
+docker build -f gitpod-neovim-tmux.dockerfile -t maxdac/gitpod-neovim:0.9.4-tmux-3.3 .
+docker tag maxdac/gitpod-neovim:0.9.4-tmux-3.3 maxdac/gitpod-neovim:latest 
 docker build -f gitpod-elixir.dockerfile -t maxdac/gitpod-elixir:1.15.6-otp-26 .
 docker tag maxdac/gitpod-elixir:1.15.6-otp-26 maxdac/gitpod-elixir:latest 
 
@@ -10,7 +10,7 @@ docker tag maxdac/gitpod-elixir:1.15.6-otp-26 maxdac/gitpod-elixir:latest
 docker login
 
 # Pushing images to docker
-docker push maxdac/gitpod-neovim:0.9.2-tmux-3.3
+docker push maxdac/gitpod-neovim:0.9.4-tmux-3.3
 docker push maxdac/gitpod-neovim:latest
 docker push maxdac/gitpod-elixir:1.15.6-otp-26 
 docker push maxdac/gitpod-elixir:latest 
