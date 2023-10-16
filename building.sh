08fe27e302172b482acde27123e6062640ba3e3c
MaxDac <massimiliano.dacunzo@hotmail.com>
2023-10-16 10:31:41 +0100

    Updated versions (#4)
    * Updating erlang/elixir versions
    
    * Update gitpod-elixir.dockerfile
    
    * Update gitpod-neovim-tmux.dockerfile
    
    * Update building.sh

--- building.sh
+++ building.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Building the images
-docker build -f gitpod-neovim-tmux.dockerfile -t maxdac/gitpod-neovim:0.9.4-tmux-3.3 .
+docker build -f gitpod-neovim-tmux.dockerfile -t maxdac/gitpod-neovim:0.9.4-tmux-3.3a .
 docker tag maxdac/gitpod-neovim:0.9.4-tmux-3.3 maxdac/gitpod-neovim:latest 
-docker build -f gitpod-elixir.dockerfile -t maxdac/gitpod-elixir:1.15.6-otp-26 .
-docker tag maxdac/gitpod-elixir:1.15.6-otp-26 maxdac/gitpod-elixir:latest 
+docker build -f gitpod-elixir.dockerfile -t maxdac/gitpod-elixir:1.15.7-otp-26 .
+docker tag maxdac/gitpod-elixir:1.15.7-otp-26 maxdac/gitpod-elixir:latest 
 
 # Logging in to docker hub
 docker login
 
 # Pushing images to docker
-docker push maxdac/gitpod-neovim:0.9.4-tmux-3.3
+docker push maxdac/gitpod-neovim:0.9.4-tmux-3.3a
 docker push maxdac/gitpod-neovim:latest
-docker push maxdac/gitpod-elixir:1.15.6-otp-26 
+docker push maxdac/gitpod-elixir:1.15.7-otp-26 
 docker push maxdac/gitpod-elixir:latest 
