d3db408b526973d1f9c6cce8c47a5ebfaa4ba53f
MaxDac <massimiliano.dacunzo@hotmail.com>
2023-11-07 11:41:55 +0000

    Adding a version for GitPod to take the change

--- building.sh
+++ building.sh
@@ -3,8 +3,8 @@
 # Building the images
 docker build -f gitpod-neovim-tmux.dockerfile -t maxdac/gitpod-neovim:0.9.4-tmux-3.3a .
 docker tag maxdac/gitpod-neovim:0.9.4-tmux-3.3a maxdac/gitpod-neovim:latest 
-docker build -f gitpod-elixir.dockerfile -t maxdac/gitpod-elixir:1.15.7-otp-26 .
-docker tag maxdac/gitpod-elixir:1.15.7-otp-26 maxdac/gitpod-elixir:latest 
+docker build -f gitpod-elixir.dockerfile -t maxdac/gitpod-elixir:1.15.7-otp-26a .
+docker tag maxdac/gitpod-elixir:1.15.7-otp-26a maxdac/gitpod-elixir:latest 
 
 # Logging in to docker hub
 docker login
@@ -12,5 +12,5 @@ docker login
 # Pushing images to docker
 docker push maxdac/gitpod-neovim:0.9.4-tmux-3.3a
 docker push maxdac/gitpod-neovim:latest
-docker push maxdac/gitpod-elixir:1.15.7-otp-26 
+docker push maxdac/gitpod-elixir:1.15.7-otp-26a 
 docker push maxdac/gitpod-elixir:latest 
