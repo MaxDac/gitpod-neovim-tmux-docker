86e4446e57bf798dcf25b691072374f917379738
MaxDac <massimiliano.dacunzo@hotmail.com>
2024-01-02 17:08:21 +0000

    Corrected version

--- building.sh
+++ building.sh
@@ -3,8 +3,8 @@
 # Building the images
 docker build -f gitpod-neovim-tmux.dockerfile -t maxdac/gitpod-neovim:0.9.5-tmux-3.3a .
 docker tag maxdac/gitpod-neovim:0.9.5-tmux-3.3a maxdac/gitpod-neovim:latest 
-docker build -f gitpod-elixir.dockerfile -t maxdac/gitpod-elixir:1.16.0-otp-26a .
-docker tag maxdac/gitpod-elixir:1.16.0-otp-26a maxdac/gitpod-elixir:latest 
+docker build -f gitpod-elixir.dockerfile -t maxdac/gitpod-elixir:1.16.0-otp-26b .
+docker tag maxdac/gitpod-elixir:1.16.0-otp-26b maxdac/gitpod-elixir:latest 
 
 # Logging in to docker hub
 docker login
@@ -12,5 +12,5 @@ docker login
 # Pushing images to docker
 docker push maxdac/gitpod-neovim:0.9.5-tmux-3.3a
 docker push maxdac/gitpod-neovim:latest
-docker push maxdac/gitpod-elixir:1.16.0-otp-26a
+docker push maxdac/gitpod-elixir:1.16.0-otp-26b
 docker push maxdac/gitpod-elixir:latest 
